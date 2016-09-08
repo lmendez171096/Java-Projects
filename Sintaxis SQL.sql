3a62796595f6dc673fb83a06fa73c0efd28ebfc5
lmendez171096 <luis_10_chatonando@hotmail.com>
2016-09-07 20:04:52 -0600

    Sintaxis

---
+++ Sintaxis SQL.sql
@@ -0,0 +1,5 @@
+/*insert into tb_peliculas(Descripcion,Clasificacion,sinopsis,actores,FechaProduccion) values ('hola','tu','tia','esta','gorda')*/
+
+/*select*from test.tb_peliculas ;*/
+/*create database Juegosjugados*/
+select*from tb_juegosjuegados
